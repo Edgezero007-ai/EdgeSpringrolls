5f3513e0d7fd66295ef82c34625914c1ff2b30f9
EdgeKurama <edgecaparoso@gmail.com>
2025-05-21 21:26:19 +0000

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# EdgeSpringrolls
-CIICC tasks
---
+++ Tasksheet/Tasksheet 1-1-1.java
@@ -0,0 +1,10 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {  // 1. Class name should be capitalized and match exactly
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // 2. Correct method name is 'println'
+                                           // 3. Missing closing quote for string
+    }
+}
