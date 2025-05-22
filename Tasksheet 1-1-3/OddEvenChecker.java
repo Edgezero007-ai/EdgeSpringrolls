8959d43bb8ade5ac0c2e8fb8ea3c8d4c853d944f
EdgeKurama <edgecaparoso@gmail.com>
2025-05-22 06:11:39 +0000

    commit

---
+++ Tasksheet 1-1-3/OddEvenChecker.java
@@ -0,0 +1,15 @@
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        int check_number = 10;  // Initialize check_number to 10
+        String message;         // Declare String variable 'message'
+
+        // Loop from 1 to check_number (inclusive)
+        for (int i = 1; i <= check_number; i++) {
+            // Check if the number is even or odd using ternary operator
+            message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+            
+            // Print the message
+            System.out.println(message);
+        }
+    }
+}
