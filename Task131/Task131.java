e63c78533ac99290199d60873595acc1b2977449
EdgeKurama <edgecaparoso@gmail.com>
2025-05-24 21:02:59 +0000

    commit

---
+++ Task131/Task131.java
@@ -0,0 +1,77 @@
+package Task131;
+
+
+class Afritada {
+    public void showIngredients() {
+        String[] Ingredients = {"Tomato Sauce", "Meat"};
+        System.out.println("Afritada = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+    }
+}
+
+class Mechado extends Afritada {
+    @Override
+    public void showIngredients() {
+        String[] Ingredients = {
+            "Tomato Sauce",
+            "Meat",
+            "Potatoes & Carrots",
+            "Tomato Paste",
+            "Liver Spread",
+            "Raisins",
+            "Hotdog",
+            "Cheese"
+        };
+        System.out.println("Mechado = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+    }
+}
+
+class Menudo extends Afritada {
+    @Override
+    public void showIngredients() {
+        String[] Ingredients = {
+            "Tomato Sauce",
+            "Meat",
+            "Potatoes & Carrots",
+            "Tomato Paste",
+            "Liver Spread",
+            "Raisins",
+            "Hotdog",
+            "Cheese"
+        };
+        System.out.println("Menudo = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+    }
+}
+
+class Caldereta extends Afritada {
+    @Override
+    public void showIngredients() {
+        String[] Ingredients = {
+            "Tomato Sauce",
+            "Meat",
+            "Potatoes & Carrots",
+            "Tomato Paste",
+            "Liver Spread",
+            "Raisins",
+            "Hotdog",
+            "Siling Labuyo",
+            "Cheese"
+        };
+        System.out.println("Caldereta = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+    }
+}
+
+public class Task131 {
+    public static void main(String[] args) {
+        // Create meal objects
+        Afritada meal1 = new Afritada();
+        Afritada meal2 = new Mechado();
+        Afritada meal3 = new Menudo();
+        Afritada meal4 = new Caldereta();
+        
+        // Display all ingredients
+        meal1.showIngredients();
+        meal2.showIngredients();
+        meal3.showIngredients();
+        meal4.showIngredients();
+    }
+}
