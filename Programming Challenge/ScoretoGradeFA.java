fc2d508fe8223e9990e5eefc8d6555c8d481b049
EdgeKurama <edgecaparoso@gmail.com>
2025-05-26 22:56:13 +0000

    commit

--- Programming Challenge/ScoretoGradeFA.java
+++ Programming Challenge/ScoretoGradeFA.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ScoreToGrade {
+public class ScoretoGradeFA {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter student's score: ");
