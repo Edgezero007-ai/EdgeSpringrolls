aa5123882fd5adc842f7e7b22e9f78a8efcbbcce
EdgeKurama <edgecaparoso@gmail.com>
2025-05-26 23:34:49 +0000

    commit

--- Task4/Task4.java
+++ Task4/Task4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindromeChecker {
+public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
