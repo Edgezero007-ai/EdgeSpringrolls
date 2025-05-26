b229985fbdb8d3e5a22ffd08994bf4883d16f9d2
EdgeKurama <edgecaparoso@gmail.com>
2025-05-26 15:30:02 +0000

    commit

---
+++ Task4/task4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        // Create a reversed version of the input string using StringBuilder
+        StringBuilder reversedBuilder = new StringBuilder(input);
+        reversedBuilder.reverse();
+        String reversed = reversedBuilder.toString();
+        
+        // Check if the input string is equal to the reversed string
+        if (input.equals(reversed)) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+        
+        scanner.close();
+    }
+}
