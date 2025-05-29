2c76238508d4ca93a4e6c8fe8703cf58056e4a08
EdgeKurama <edgecaparoso@gmail.com>
2025-05-29 03:13:45 +0000

    commit

---
+++ Gorilla.java
@@ -0,0 +1,20 @@
+public class Gorilla implements Animal {
+    public boolean feed(boolean timeToEat) {
+        // put gorilla food into cage
+        if (timeToEat) {
+            System.out.println("Feeding the gorilla...");
+            return true;
+        }
+        return false;
+    }
+
+    public void groom() {
+        // lather, rinse, repeat
+        System.out.println("Grooming the gorilla...");
+    }
+
+    public void pet() {
+        // pet at your own risk
+        System.out.println("Attempting to pet the gorilla...");
+    }
+}
